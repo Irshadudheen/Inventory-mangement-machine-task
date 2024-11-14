366a61d719a311a6a8ad5e32da0d940555aa6ef0
Irshadudheen <146156401+Irshadudheen@users.noreply.github.com>
2024-11-15 00:04:12 +0530

    feat:folder structure of client side has been end

---
+++ Inventory/src/main.tsx
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+
+import {store,persistor} from './redux/storage.ts'
+import { PersistGate } from 'redux-persist/integration/react'
+ReactDOM.createRoot(document.getElementById('root')!).render(
+
+     <Provider store={store}>
+      <PersistGate persistor={persistor}>
+      
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+        
+        </PersistGate>
+     </Provider>
+
+)
